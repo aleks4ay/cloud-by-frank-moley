a802d2f89304cff16f64e61254140f77f587b939
Oleksii Sergiienko <aleks4ay@gmail.com>
2022-03-23 14:31:50 +0200

    Added Spring-Boot Cloud Config Server.

---
+++ config-server/src/test/java/org/aleks4ay/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aleks4ay.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
